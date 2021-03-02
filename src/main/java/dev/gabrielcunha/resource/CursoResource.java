2cf15f6680cf0e3d3949cb73beb942ffbfdc7174
Gabriel Cunha <gabrielcunhadev@gmail.com>
2021-03-02 01:20:39 -0300

    Adicionado método para atualizar o recurso Pessoa. Alterado o método para criar a pessoa.

--- src/main/java/dev/gabrielcunha/resource/CursoResource.java
+++ src/main/java/dev/gabrielcunha/resource/CursoResource.java
@@ -1,73 +1,13 @@
 package dev.gabrielcunha.resource;
 
-import java.net.URI;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import dev.gabrielcunha.entity.Curso;
-import dev.gabrielcunha.repository.CursoRepo;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/cursos")
 public class CursoResource {
 
-	@Autowired
-	private CursoRepo cursoRepo;
 	
-	@ApiOperation(value="Cria ou atualiza um recurso.")	
-	@ApiResponses(value = { 
-	        @ApiResponse(code = 201, message = "Recurso criado."), 
-	        @ApiResponse(code = 400, message = "Bad Request - Recurso Inválido.")})		
-	@PostMapping
-	public ResponseEntity<Curso> criar(@RequestBody Curso curso) {
-		
-		Curso cursoCriado = cursoRepo.save(curso);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
-											 .path("/{id}")
-											 .buildAndExpand(curso.getId()).toUri();
-		
-		return ResponseEntity.created(uri).body(cursoCriado);
-	}	
-
-    @ApiParam(name = "id",
-    		  value = "Identificador do recurso a ser encontrado. Não pode estar vazio.",
-			  example = "72",
-			  required = true)	
-	@ApiResponses(value = {
-	        @ApiResponse(code = 200, message = "Sucesso.", response = Curso.class),
-	        @ApiResponse(code = 404, message = "Recurso não encontrado.") 
-	})	
-	@GetMapping("/{id}")
-	public ResponseEntity<Curso> buscar(@PathVariable Long id) {
-		
-    	Optional<Curso> curso = cursoRepo.findById(id);
-		
-		if (curso.isPresent()) {
-			return ResponseEntity.ok(curso.get());
-		} else {
-			return ResponseEntity.notFound().build();
-		}
-	}	
-    
-    @DeleteMapping("/{id}")
-    public void deletar(@PathVariable("id") Long id) {
-    	cursoRepo.deleteById(id);
-    }    
-		
+   
 	
 }
--- src/main/java/dev/gabrielcunha/resource/PessoaResource.java
+++ src/main/java/dev/gabrielcunha/resource/PessoaResource.java
@@ -3,6 +3,7 @@ package dev.gabrielcunha.resource;
 import java.net.URI;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import dev.gabrielcunha.entity.Curso;
 import dev.gabrielcunha.entity.Pessoa;
 import dev.gabrielcunha.repository.PessoaRepo;
 import io.swagger.annotations.ApiOperation;
@@ -33,37 +34,57 @@ public class PessoaResource {
 	
 	@ApiOperation(value="Cria ou atualiza um recurso.")	
 	@ApiResponses(value = { 
-	        @ApiResponse(code = 201, message = "Recurso criado ou atualizado."), 
-	        @ApiResponse(code = 400, message = "Bad Request - Recurso Inválido.")})		
+	        @ApiResponse(code = 201, message = "Recurso criado."), 
+	        @ApiResponse(code = 409, message = "Recurso já existe.")})		
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@RequestBody Pessoa pessoa) {
 		
 		Pessoa pessoaDoBancoDados = pessoaRepo.findByCpf(pessoa.getCpf());
 		
-		Pessoa pessoaCriadaOuAtualizada = new Pessoa();
-		
 		if (!ObjectUtils.isEmpty(pessoaDoBancoDados)) {
-			pessoaDoBancoDados.setCpf(pessoa.getCpf());
-			pessoaDoBancoDados.setNome(pessoa.getNome());
-			pessoaDoBancoDados.setEmail(pessoa.getEmail());
-			pessoaCriadaOuAtualizada = pessoaRepo.save(pessoaDoBancoDados);
-		} else {
-			pessoaCriadaOuAtualizada = pessoaRepo.save(pessoa);
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
+		
+		Pessoa pessoaCriada = pessoaRepo.save(pessoa);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
 											 .path("/{id}")
 											 .buildAndExpand(pessoa.getId()).toUri();
 		
-		return ResponseEntity.created(uri).body(pessoaCriadaOuAtualizada);
+		return ResponseEntity.created(uri).body(pessoaCriada);
 	}	
+	
+	
+	@ApiOperation(value="Endpoint responsável por atualizar um recurso pelo id, enviando no "
+			+ "body da requisição o json do recurso com os dados a serem atualizados.")	
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Sucesso."),
+            @ApiResponse(code = 400, message = "Requisição do cliente enviada com erro."),
+            @ApiResponse(code = 404, message = "Recurso não encontrado.")
+    	})	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Pessoa pessoa) {
+		
+		Optional<Pessoa> pessoaOptional = pessoaRepo.findById(id);
+		
+		if (!pessoaOptional.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Pessoa pessoaBanco = pessoaOptional.get();
+		
+		BeanUtils.copyProperties(pessoa, pessoaBanco, "id");
+		
+		pessoaRepo.save(pessoaBanco);
+		
+		return ResponseEntity.ok(pessoaBanco);
+	}
+		
 
-    @ApiParam(name = "id",
-			  value = "Identificador do recurso a ser encontrado. Não pode estar vazio.",
-			  example = "72",
-			  required = true)	
+    @ApiParam(name = "id",  value = "Identificador do recurso a ser encontrado. Não pode ser vazio.",
+			  example = "72", required = true)	
 	@ApiResponses(value = {
-	        @ApiResponse(code = 200, message = "Sucesso.", response = Curso.class),
+	        @ApiResponse(code = 200, message = "Sucesso.", response = Pessoa.class),
 	        @ApiResponse(code = 404, message = "Recurso não encontrado.") 
 	})
 	@GetMapping("/{id}")
@@ -73,13 +94,22 @@ public class PessoaResource {
 		
 		if (pessoa.isPresent()) {
 			return ResponseEntity.ok(pessoa.get());
-		} else {
-			return ResponseEntity.notFound().build();
-		}
+		} 
+		
+		return ResponseEntity.notFound().build();
 	}	
     
+    @ApiParam(name = "id",
+			  value = "Identificador do recurso a ser excluido. Não pode ser vazio.",
+			  example = "12",
+			  required = true)	
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Sucesso.", response = Pessoa.class),
+	        @ApiResponse(code = 404, message = "Recurso não encontrado.") 
+	})    
     @DeleteMapping("/{id}")
     public ResponseEntity<?> excluir(@PathVariable("id") Long id) {
+    	
     	Optional<Pessoa> pessoaBanco = pessoaRepo.findById(id);
     	
     	if (!pessoaBanco.isPresent()) {
